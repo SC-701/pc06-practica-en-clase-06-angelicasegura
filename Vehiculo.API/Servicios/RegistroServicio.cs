673533f24748b83f65eaeaa106f92df4977a7df2
angelicasegura <angelicaseguralopez@gmail.com>
2026-02-25 19:00:36 -0600

    archivos
    archivos

---
+++ Vehiculo.API/Servicios/RegistroServicio.cs
@@ -0,0 +1,14 @@
+﻿using Abstracciones.Interfaces.Servicios;
+using Abstracciones.Modelos.Servicios.Registro;
+
+namespace Servicios
+{
+    private readonly IConfiguracion
+    public class RegistroServicio : IRegistroServicio
+    {
+        public Task<Propietario> Obtener(string placa)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
