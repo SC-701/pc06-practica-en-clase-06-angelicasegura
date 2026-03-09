88a0e52fcc532ed4376e3286e07d05af0446e719
angelicasegura <angelicaseguralopez@gmail.com>
2026-03-09 13:48:16 -0600

    correciones

--- Vehiculo.API/API/Controllers/VehiculoController.cs
+++ Vehiculo.API/API/Controllers/VehiculoController.cs
@@ -28,8 +28,8 @@ namespace API.Controllers
         [HttpPut("{Id}")]
         public async Task<IActionResult> Editar([FromRoute]Guid Id, [FromBody]VehiculoRequest vehiculo)
         {
-           if ( !await VerificarVehiculoExiste(Id));
-                return NotFound("El vehiculo  no existe");
+           if ( !await VerificarVehiculoExiste(Id))
+            return NotFound("El vehiculo  no existe");
             var resultado = await _vehiculoFlujo.Editar(Id, vehiculo);
             return Ok(resultado);
         }
@@ -39,7 +39,7 @@ namespace API.Controllers
         [HttpDelete("{Id}")]
         public async Task<IActionResult> Eliminar([FromRoute] Guid Id)
         {
-            if (!await VerificarVehiculoExiste(Id)) ;
+            if (!await VerificarVehiculoExiste(Id)) 
                 return NotFound("El vehiculo  no existe");
             var resultado = await _vehiculoFlujo.Eliminar(Id);
             return NoContent();
@@ -52,9 +52,8 @@ namespace API.Controllers
                 return NoContent();
             return Ok(resultado);
         }
-        //[HttpGet ("{Id}")]
-        [HttpGet]
-        [Route("Obtener")]
+        [HttpGet ("{Id}")]
+       
         public async Task<IActionResult> Obtener([FromHeader] Guid Id)
         {
             var resultado = await _vehiculoFlujo.Obtener(Id);
